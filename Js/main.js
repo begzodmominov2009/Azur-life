94532c1a83887f01bfa5e3d7d2c842234f3a5a82
begzodmominov2009 <begzodmominov32@gmail.com>
2025-08-21 12:22:40 +0300

    adding about

---
+++ Js/main.js
@@ -0,0 +1,34 @@
+const header = document.getElementById("header");
+
+window.addEventListener("scroll", () => {
+    if (scrollY > 120) {
+        header.classList.add("shrink")
+    } else {
+        header.classList.remove("shrink")
+    }
+
+})
+
+$(document).ready(function () {
+    $(".owl-carousel").owlCarousel();
+});
+
+$(".owl-carousel").owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: true,
+    dots: false,
+    autoplay: true,
+    autoplayTimeout: 1000,
+    responsive: {
+        0: {
+            items: 1,
+        },
+        600: {
+            items: 1,
+        },
+        1000: {
+            items: 2,
+        },
+    },
+});
